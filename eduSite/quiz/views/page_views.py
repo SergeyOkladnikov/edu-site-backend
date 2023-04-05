21011160e4afc894389663c205f8bc2887bc4e60
SergeyOkladnikov <mr.okladnicoff@yandex.ru>
2023-04-05 17:26:12 +0500

    Начало работы с блоком создания quiz

--- eduSite/quiz/views/page_views.py
+++ eduSite/quiz/views/page_views.py
@@ -12,18 +12,18 @@ def quiz_constructor(request):
         'title': 'Создание Quiz'
     }
 
-    return render(request, 'quiz/index.html', context=context)
+    return render(request, 'quiz/quiz-constructor.html', context=context)
 
 def quiz_preview(request):
     context = {
         'title': 'Предпросмотр Quiz'
     }
 
-    return render(request, 'quiz/index.html', context=context)
+    return render(request, 'quiz/quiz-preview.html', context=context)
 
 def quiz_solving(request):
     context = {
         'title': 'Quiz'
     }
 
-    return render(request, 'quiz/index.html', context=context)
+    return render(request, 'quiz/quiz-solving.html', context=context)
