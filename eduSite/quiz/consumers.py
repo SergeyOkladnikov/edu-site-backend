2acd7bf4d07661744a9d06fff29cbc5559a47f5a
SergeyOkladnikov <mr.okladnicoff@yandex.ru>
2023-06-11 17:21:09 +0500

    Больше инфы в сообщении об ответе

--- eduSite/quiz/consumers.py
+++ eduSite/quiz/consumers.py
@@ -28,12 +28,14 @@ class QuizConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         name = text_data_json['name']
-        question = text_data_json['question']
+        question_pk = text_data_json['question_pk']
+        question_text = text_data_json['question_text']
+        question_number = text_data_json['question_number']
         chosen_answers = text_data_json['chosen_answers']
         is_correct = True
         connection_code = self.room_name
         print(text_data_json)
-        for a in get_object_or_404(Question, pk=question).answers.all():
+        for a in get_object_or_404(Question, pk=question_pk).answers.all():
             if a.pk in chosen_answers and not a.is_correct:
                 is_correct = False
                 break
@@ -45,17 +47,23 @@ class QuizConsumer(WebsocketConsumer):
             {
                 'type': 'answer_data',
                 'name': name,
-                'question': question,
+                'question_pk': question_pk,
+                'question_text': question_text,
+                'question_number': question_number,
                 'is_correct': is_correct
             }
         )
 
     def answer_data(self, event):
         name = event['name']
-        question = event['question']
+        question_pk = event['question_pk']
+        question_text = event['question_text']
+        question_number = event['question_number']
         is_correct = event['is_correct']
         self.send(text_data=json.dumps({
             'name': name,
-            'question': question,
+            'question_pk': question_pk,
+            'question_text': question_text,
+            'question_number': question_number,
             'is_correct': is_correct
         }))
